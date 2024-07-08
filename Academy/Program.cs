9242e143d5079939dbb901ec93ebeab501d63c64
Кристина Волосова <justanobody017527@gmail.com>
2024-07-08 13:05:44 +0300

    Homework DONE, quite poorly though.

--- Academy/Program.cs
+++ Academy/Program.cs
@@ -98,19 +98,104 @@ Human human = new Human("Vercetti", "Tommy", 30);
 				new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
 			};
 			//Specialization
-			for (int i = 0; i < group.Length; i++)
+			//Print(group);
+			string cmd = "group.txt";
+			//Save(cmd, group);
+			Print(Load(cmd));
+			//System.Diagnostics.Process.Start("notepad", cmd);
+			//TODO:
+			//1.Код, выводящий группу на экран вынести в метод ??? Print(???);
+			//2.Код, сохраняющий группу в файл вынести в метод ??? Save(???);
+			//3.Написать метод ??? Load(???), который загружает группу из файла, в такой же массив, как создан в методе Main();
+		}
+		static void Print(Human[] array)
+		{
+			for (int i = 0; i < array.Length; i++)
 			{
-				Console.WriteLine(group[i].ToString());
+				Console.WriteLine(array[i].ToString());
 				Console.WriteLine(delimeter);
 			}
-			StreamWriter writer = new StreamWriter("group.txt"); //Создаем и открываем поток
-			for (int i = 0; i < group.Length; i++)
+		}
+		static void Save(string fileName, Human[] array)
+		{
+			if (!fileName.EndsWith(".txt"))
 			{
-				writer.WriteLine(group[i]);
+				fileName += ".txt";
+			}
+			StreamWriter writer = new StreamWriter(fileName); //Создаем и открываем поток
+			for (int i = 0; i < array.Length; i++)
+			{
+				writer.WriteLine(array[i]);
 			}
 			writer.Close();
-			string cmd = "group.txt";
-			System.Diagnostics.Process.Start("notepad", cmd);
+		}
+		static Human[] Load(string fileName)
+		{
+            Console.WriteLine("-----LOAD-----");
+            if (File.Exists(fileName))
+			{
+				StreamReader reader = new StreamReader(fileName);
+				string[] fileContents = File.ReadAllLines(fileName);
+				Human[] group = new Human[fileContents.Length];
+				string substring;
+				for (int i = 0; i < fileContents.Length; i++)
+				{
+					substring = String.Join(" ", fileContents[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+					string[] subarr = substring.Split(' ');
+
+					if (subarr[0] == "Academy.Student:")
+					{
+						group[i] = new Student(subarr[1], subarr[2], Convert.ToInt32(subarr[3]), subarr[4], subarr[5], Convert.ToDouble(subarr[6]), Convert.ToDouble(subarr[7]));
+					}
+					else if (subarr[0] == "Academy.Teacher:")
+					{
+						string name = subarr[1];
+						string surname = subarr[2];
+						int age = Convert.ToInt32(subarr[3]);
+
+						int index = 4;
+						string speciality = "";
+						while (!int.TryParse(subarr[index], out _))//out _ потому что мы не сохраняем результат парсинга.
+						{
+							speciality += subarr[index] + " ";
+							index++;
+						}
+						speciality = speciality.Trim();
+						int experience = Convert.ToInt32(subarr[index]);
+						group[i] = new Teacher(name, surname, age, speciality, experience);
+					}
+					else if (subarr[0] == "Academy.Graduate:")
+					{
+						string name = subarr[1];
+						string surname = subarr[2];
+						int age = Convert.ToInt32(subarr[3]);
+						string speciality = subarr[4];
+						string studyGroup = subarr[5];
+						double rating = Convert.ToDouble(subarr[6]);
+						double attendance = Convert.ToDouble(subarr[7]);
+
+						int index = 8;
+						string subject = "";
+						while (index < subarr.Length)
+						{
+							subject += subarr[index] + " ";
+							index++;
+						}
+						subject = subject.Trim();
+						group[i] = new Graduate(name, surname, age, speciality, studyGroup, rating, attendance, subject);
+					}
+				}
+				reader.Close();
+				Console.WriteLine("-----LOAD DONE-----");
+				return group;
+
+			}
+			else 
+			{
+                Console.WriteLine("Такого файла не существует.");
+				Console.WriteLine("-----LOAD DONE-----");
+				return null;
+            }
 			
 		}
 	}
