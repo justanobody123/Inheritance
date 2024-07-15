2365d6b3d1b0de4841c08d6eededa2d176c94a67
Кристина Волосова <justanobody017527@gmail.com>
2024-07-15 21:49:51 +0300

    Stack DONE

---
+++ DataContainers/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContainers
+{
+	internal class Node<X>
+	{
+		public X Data { get; set; }
+		public Node<X> Next { get; set; }
+        public Node(X value)
+        {
+            Data = value;
+            Next = null;
+        }
+    }
+}
--- DataContainers/Program.cs
+++ DataContainers/Program.cs
@@ -10,8 +10,27 @@ namespace DataContainers
 	{
 		static void Main(string[] args)
 		{
-			
+			try
+			{
+				MyStack<int> stack = new MyStack<int>();
+				for (int i = 1; i <= 5; i++)
+				{
+					stack.Push(i * 10);
+				}
+				Console.WriteLine(stack.Pop());//50
+				Console.WriteLine(stack.Pop());//40
+				Console.WriteLine(stack.Pop());//30
+				Console.WriteLine(stack.Peek());//50
+				Console.WriteLine(stack.Pop());//20
+				stack.Clear();
+				Console.WriteLine(stack.Peek());//Эксепшен
+			}
+			catch (Exception e)
+			{
 
-		}
+                Console.WriteLine(e.Message);
+            }
+			
+        }
 	}
 }
